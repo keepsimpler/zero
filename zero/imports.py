733e5dc98438813b6940761078f3d5eb6e4fcda6
fengwf <fengwenfeng@gmail.com>
2019-11-01 16:25:29 +0800

    remove import pytorch_lightning.

--- zero/imports.py
+++ zero/imports.py
@@ -9,7 +9,7 @@ import torch.utils.checkpoint as cp # checkpointing
 # import torch.nn.functional as F
 torch.backends.cudnn.benchmark = True
 
-import pytorch_lightning as pl
+# import pytorch_lightning as pl
 
 import numpy as np
 from numpy.linalg import matrix_power # for calculation of paths
